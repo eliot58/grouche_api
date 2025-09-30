e92cd0f217b75f112d2b130450e1d06ce7da3fce
Aiomedin Chorobaev <eliothab58@gmail.com>
2025-10-01 05:07:32 +0600

    fix

--- src/main.ts
+++ src/main.ts
@@ -40,6 +40,7 @@ async function bootstrap() {
       'https://testnet.grouche.com',
       'https://miniapp.grouche.com',
       'https://dev.grouche.com',
+      'http://dev.grouche.com:3000',
       'http://0.0.0.0:3000',
       'http://localhost:3000',
       'http://127.0.0.1:3000',
