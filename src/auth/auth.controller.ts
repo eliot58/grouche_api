8b206a8f1d5cd9560661ebcc71ba9c3c30d2d4db
Aiomedin Chorobaev <eliothab58@gmail.com>
2025-10-01 02:11:57 +0600

    fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -39,7 +39,7 @@ export class AuthController {
     res.setCookie('auth_token', token, {
       httpOnly: true,
       secure: false,
-      sameSite: 'lax',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
