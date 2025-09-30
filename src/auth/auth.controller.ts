6bc000620628a29213ff7a6d94019769e44ec3a8
Aiomedin Chorobaev <eliothab58@gmail.com>
2025-10-01 04:53:54 +0600

    fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -38,7 +38,8 @@ export class AuthController {
 
     res.setCookie('auth_token', token, {
       path: "/",
-      domain: ".grouche.com",
+      // domain: ".grouche.com",
+      domain: "localhost",
       httpOnly: true,
       secure: true,  
       sameSite: "none",
